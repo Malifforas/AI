cdc82a1d764093edeb0351a2de2d4f948199294e
Malifforas <122689565+Malifforas@users.noreply.github.com>
2023-03-12 22:57:14 -0400

    Add files via upload

--- data_reader.py
+++ data_reader.py
@@ -1,13 +1,48 @@
-import os
 import json
+import os
 
-data_dir = r'C:\Users\Blizzard\Desktop\AI\pokegold-master'
 
-with open(os.path.join(data_dir, 'moves.json')) as f:
-    moves = json.load(f)
+class DataReader:
+    def __init__(self):
+        self.ROOT_DIR = os.path.abspath(os.curdir)
+        self.POKEDEX = self.read_json_file('pokedex.json')
+        self.MOVES = self.read_json_file('moves.json')
+        self.TYPE_CHART = self.read_json_file('type_chart.json')
+        self.GYM_LEADERS = self.read_json_file('gym_leaders.json')
+        self.ELITE_FOUR = self.read_json_file('elite_four.json')
+        self.CHAMPION = self.read_json_file('champion.json')
+        self.LEVEL_LIMITS = self.read_json_file('level_limits.json')
 
-with open(os.path.join(data_dir, 'types.json')) as f:
-    types = json.load(f)
+    def read_json_file(self, filename):
+        filepath = os.path.join(self.ROOT_DIR, 'data', filename)
+        with open(filepath, 'r') as f:
+            return json.load(f)
 
-with open(os.path.join(data_dir, 'pokemon.json')) as f:
-    pokemon = json.load(f)
+    def get_pokemon_by_id(self, pokemon_id):
+        return self.POKEDEX[str(pokemon_id)]
+
+    def get_pokemon_by_name(self, name):
+        for pokemon in self.POKEDEX.values():
+            if pokemon['name'].lower() == name.lower():
+                return pokemon
+        return None
+
+    def get_move_by_id(self, move_id):
+        return self.MOVES[str(move_id)]
+
+    def get_gym_leader_by_id(self, gym_leader_id):
+        return self.GYM_LEADERS[str(gym_leader_id)]
+
+    def get_elite_four_member_by_id(self, elite_four_member_id):
+        return self.ELITE_FOUR[str(elite_four_member_id)]
+
+    def get_champion(self):
+        return self.CHAMPION
+
+    def get_level_limit_for_gym(self, gym_name):
+        if gym_name.lower() in self.LEVEL_LIMITS:
+            return self.LEVEL_LIMITS[gym_name.lower()]
+        return None
+
+    def get_type_chart(self):
+        return self.TYPE_CHART
--- q_learning.py
+++ q_learning.py
@@ -1,31 +1,88 @@
-import numpy as np
+import os
+import pickle
 import random
+from collections import defaultdict
 
-class QLearning:
-    def __init__(self, actions, learning_rate=0.1, discount_factor=0.9, exploration_rate=1.0, exploration_decay_rate=0.99):
-        self.actions = actions
+from game import Game
+
+
+class QLearningAgent:
+    def __init__(self, actions, learning_rate=0.1, discount_factor=0.99, epsilon=1.0, epsilon_decay=0.99, epsilon_min=0.01):
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
-        self.exploration_rate = exploration_rate
-        self.exploration_decay_rate = exploration_decay_rate
-        self.q_table = {}
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.actions = actions
+        self.q_table = defaultdict(lambda: [0.0] * len(self.actions))
 
-    def get_q_value(self, state, action):
-        if state not in self.q_table:
-            self.q_table[state] = np.zeros(len(self.actions))
-        return self.q_table[state][action]
-
-    def update_q_value(self, state, action, reward, next_state):
-        current_q = self.get_q_value(state, action)
-        next_q = max([self.get_q_value(next_state, a) for a in self.actions])
-        new_q = current_q + self.learning_rate * (reward + self.discount_factor * next_q - current_q)
-        self.q_table[state][action] = new_q
-
-    def choose_action(self, state, explore=True):
-        if explore and random.random() < self.exploration_rate:
+    def choose_action(self, state):
+        if random.uniform(0, 1) < self.epsilon:
+            # Choose a random action
             return random.choice(self.actions)
         else:
-            return np.argmax(self.q_table[state])
+            # Choose the best action based on the Q-table
+            return self.actions[self.q_table[state].index(max(self.q_table[state]))]
 
-    def decay_exploration_rate(self):
-        self.exploration_rate *= self.exploration_decay_rate
+    def update_q_table(self, state, action, reward, next_state, done):
+        # Update Q-value of the state-action pair using the Q-learning update rule
+        current_q = self.q_table[state][action]
+        if done:
+            # If the game is over, the next max Q-value is 0
+            next_max_q = 0
+        else:
+            # Otherwise, use the max Q-value of the next state
+            next_max_q = max(self.q_table[next_state])
+        new_q = current_q + self.learning_rate * (reward + self.discount_factor * next_max_q - current_q)
+        self.q_table[state][action] = new_q
+
+    def decay_epsilon(self):
+        self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
+
+    def save_q_table(self, file_path):
+        full_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'AI', 'pokegold-master', file_path)
+        with open(full_path, 'wb') as f:
+            pickle.dump(dict(self.q_table), f)
+
+    def load_q_table(self, file_path):
+        full_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'AI', 'q_tables', file_path)
+        with open(full_path, 'rb') as f:
+            self.q_table = defaultdict(lambda: [0.0] * len(self.actions), pickle.load(f))
+
+
+class QLearning:
+    def __init__(self, game: Game):
+        self.game = game
+        self.agent = QLearningAgent(game.get_actions())
+        self.num_episodes = 1000
+
+    def train(self):
+        for i in range(self.num_episodes):
+            self.game.start()
+            state = self.game.get_state()
+            done = False
+            while not done:
+                action = self.agent.choose_action(state)
+                self.game.take_action(action)
+                next_state = self.game.get_state()
+                reward = self.game.get_reward()
+                self.agent.update_q_table(state, action, reward, next_state)
+                state = next_state
+                if self.game.game_over:
+                    done = True
+                    break
+            self.agent.decay_epsilon()
+
+    def play(self):
+        self.game.start()
+        state = self.game.get_state()
+        while not self.game.game_over:
+            action = self.agent.choose_action(state)
+            self.game.take_action(action)
+            state = self.game.get_state()
+
+    def save_q_table(self, file_path):
+        self.agent.save_q_table(file_path)
+
+    def load_q_table(self, file_path):
+        self.agent.load_q_table(file_path)
